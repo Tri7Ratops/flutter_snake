8c88da1dd5fa591d6ae45cae8a53b5ff1900a1d2
Martin JNR <pro.martin.januario@gmail.com>
2021-06-27 22:35:40 +0200

    [+] Screen repository

---
+++ lib/screens/home/views/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: ,
+      ),
+    );
+  }
+}
